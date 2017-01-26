6fbfe50272c026d1f424b0c40ec04f4d71942f61
STEPHANIETAN <STEPHANIETANLF@GMAIL.COM>
2017-01-26 17:17:18 +0800

    commit the third time

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var bcrypt = require('bcrypt');
+var bcrypt = require('bcryptjs');
 
 //email validation
 var emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
